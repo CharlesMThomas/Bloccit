1b72877702cf97774a8f28ef88e9bc9db8e5376b
Charles Thomas <charles@charlesmthomas.com>
2015-11-17 22:52:25 -0500

    Assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,2 @@
-include RandomData
-
-# Create Posts
-50.times do
-  Post.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
-  )
-end
-posts = Post.all
-
-# Create Comments
-100.times do
-  Comment.create(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-  )
-end
-
-puts "Seed Finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+# Create Unique Post
+Post.find_or_create_by(title: "My Unique Post", body: 'Unique Post Content')
