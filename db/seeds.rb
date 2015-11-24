36565ac723b2d952e4b7a65b4e168f7e4987a4f9
Charles Thomas <charles@charlesmthomas.com>
2015-11-24 18:02:47 -0500

    Assignment 34 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,18 @@ posts = Post.all
   )
 end
 
+# Create Sponsored Posts
+50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: 99
+  )
+end
+
 puts "Seed Finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
