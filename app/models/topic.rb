7cb196841e155c2708a9d63da2930a1b7b28d4e8
Charles Thomas <charles@charlesmthomas.com>
2015-12-09 23:16:26 -0500

    Assignment-46-completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,8 +3,9 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
-  scope :publicly_viewable, -> { where(public: true) }
-  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+  scope :publicaly_viewable, -> { where(public: true) }
+  scope :visible_to, -> (user) { user ? all : publicaly_viewable }
+  scope :privately_viewable, -> { where(public: false) }
 
 
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -25,4 +25,37 @@ RSpec.describe Topic, type: :model do
       expect(topic.public).to be(true)
     end
   end
+
+  context "scopes" do
+    before do
+      @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+      @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
+    end
+
+    describe "publicaly_viewable" do
+      it "should only show pulic topics" do
+        expect(Topic.publicaly_viewable).to eq([@public_topic])
+      end
+    end
+
+    describe "privately_viewable" do
+      it "should only show private topics" do
+        expect(Topic.privately_viewable).to eq([@private_topic])
+      end
+    end
+
+    describe "visible_to(user)" do
+      it "should show all topics when user is signed in" do
+        user = create(:user)
+        expect(Topic.visible_to(user)).to eq(Topic.all)
+      end
+    end
+
+    describe "visible_to(nil)" do
+      it "should only show public topcs when no user is signed in" do
+        user = nil
+        expect(Topic.visible_to(user)).to eq([@public_topic])
+      end
+    end
+  end
 end
